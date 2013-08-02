d252e4ed11c89862dcbc28df18f2bfadcc4d90f3
Captain Shadows <captain_shadows@aol.com>
2013-08-02 19:12:27 -0400

    15 min commit

--- src/main/java/ccm/trade_stuffs/utils/helper/JavaHelper.java
+++ src/main/java/ccm/trade_stuffs/utils/helper/JavaHelper.java
@@ -17,8 +17,28 @@ public final class JavaHelper
     {
         if (input.contains(s))
         {
-            final String finished = input.substring(0, 1).toUpperCase();
-            input.indexOf(s);
+            final StringBuilder finished = new StringBuilder();
+            int lastSpace = -1;
+            String currentString = input;
+            for (int i = 0; i < input.length(); i++)
+            {
+                final int first = lastSpace + 1;
+                final int last = lastSpace - 1;
+
+                finished.append(currentString.substring(first, 1).toUpperCase());
+                lastSpace = currentString.indexOf(s);
+                finished.append(currentString.substring(1, lastSpace - 1));
+                finished.append(s);
+                if (!(last <= currentString.length()))
+                {
+                    currentString = currentString.substring(last, currentString.length());
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return finished.toString();
         }
         else
         {
