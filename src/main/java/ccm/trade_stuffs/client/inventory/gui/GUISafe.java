762659d6e4267836e2712bbc20f888a1b8f42f23
Captain Shadows <captain_shadows@aol.com>
2013-08-04 01:38:24 -0400

    15 mins Auto Commit for Trade Stuffs

--- src/main/java/ccm/trade_stuffs/client/inventory/gui/GUISafe.java
+++ src/main/java/ccm/trade_stuffs/client/inventory/gui/GUISafe.java
@@ -3,6 +3,9 @@
  */
 package ccm.trade_stuffs.client.inventory.gui;
 
+import org.lwjgl.opengl.GL11;
+
+import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.entity.player.InventoryPlayer;
 
@@ -11,6 +14,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 
 import ccm.trade_stuffs.inventory.ContainerSafe;
 import ccm.trade_stuffs.tileentity.Safe;
+import ccm.trade_stuffs.utils.lib.Guis;
 
 /**
  * GUITrade
@@ -21,21 +25,44 @@ import ccm.trade_stuffs.tileentity.Safe;
 @SideOnly(Side.CLIENT)
 public class GUISafe extends GuiContainer
 {
-    private final Safe safe;
-
     /**
      * @param container
      */
     public GUISafe(final InventoryPlayer player, final Safe tile)
     {
         super(new ContainerSafe(player, tile));
-        safe = tile;
+    }
+
+    @Override
+    public void initGui()
+    {
+        super.initGui();
+        buttonList.clear();
+        int i = 0;
+        for (int row = 0; row < 3; row++)
+        {
+            for (int column = 0; column < 3; column++)
+            {
+                buttonList.add(new GuiButton(i,
+                                             ((22 * row) + guiLeft) + 27,
+                                             ((22 * column) + guiTop) + 38,
+                                             20,
+                                             20,
+                                             "" + i));
+                i += 1;
+            }
+        }
     }
 
     @Override
     protected void drawGuiContainerBackgroundLayer(final float opacity, final int x, final int y)
     {
-        // TODO Auto-generated method stub
+        GL11.glColor4f(1, 1, 1, 1);
 
+        mc.func_110434_K().func_110577_a(Guis.TEXTURE_GUI_SAFE);
+
+        final int xStart = (width - xSize) / 2;
+        final int yStart = (height - ySize) / 2;
+        drawTexturedModalRect(xStart, yStart, 0, 0, xSize, ySize);
     }
 }
