a7f492b5a0988f9ef587f9a66ae91a7a2f83f7e5
ElConquistador <itsme.elconquistador@gmail.com>
2013-08-05 12:48:30 +0200

    Start working on new bank system

--- src/main/java/ccm/trade_stuffs/bank/BankAccount.java
+++ src/main/java/ccm/trade_stuffs/bank/BankAccount.java
@@ -14,89 +14,15 @@ public class BankAccount {
 
 	public String player;
 
-	public HashMap<CoinType, Integer> coins = new HashMap<CoinType, Integer>();
-	public ArrayList<ItemStack> items = new ArrayList<ItemStack>();
-
-	private int coinBalance = 0;
-
 	public BankAccount(String player) {
 		this.player = player;
 	}
-
-	public void addCoins(CoinType coinType, int amount) {
-		int balance = 0;
-		if(coins.containsKey(coinType)) {
-			balance = coins.get(coinType);
-		}
-		coins.put(coinType, balance + amount);
-		countCoinBalance();
-	}
-
-	public void removeCoins(CoinType coinType, int amount) {
-		int balance = 0;
-		if(coins.containsKey(coinType)) {
-			balance = coins.get(coinType);
-		}
-		coins.put(coinType, (int) Math.min(0, balance - amount));
-		countCoinBalance();
-	}
-
-	public void addItemStack(ItemStack stack) {
-		boolean found = false;
-		for(ItemStack item : items) {
-			if(item != null && InventoryHelper.areItemStacksEqualWithoutSize(stack, item)) {
-				stack.stackSize += item.stackSize;
-				found = true;
-			}
-		}
-		if(!found) {
-			items.add(stack);
-		}
-	}
-
-	public void removeItemStack(ItemStack stack) {
-		for(ItemStack item : items) {
-			if(item != null && InventoryHelper.areItemStacksEqualWithoutSize(stack, item)) {
-				stack.stackSize -= item.stackSize;
-			}
-		}
-	}
-
-	public void countCoinBalance() {
-		coinBalance = 0;
-		for(CoinType coinType : coins.keySet()) {
-			coinBalance += (coinType.getValue() * coins.get(coinType));
-		}
-	}
-
-	public int getCoinBalance() {
-		return coinBalance;
-	}
 	
 	public void readFromNBT(NBTTagCompound nbt) {		
-		player = nbt.getString("Player");
-		
-		coins = new HashMap<CoinType, Integer>();
-		for(CoinType coinType : CoinTypes.getTypes()) {
-			if(nbt.hasKey("Coin-" + coinType.getName())) {
-				coins.put(coinType, nbt.getInteger("Coin-" + coinType.getName()));
-			}
-		}
-		countCoinBalance();
-		
-		NBTTagList list = nbt.getTagList("Items");
-		ItemStack[] stacks = InventoryHelper.readInventoryFromNBT(list, list.tagCount());		
-		for(int i = 0; i < stacks.length; i++) {
-			items.add(stacks[i]);
-		}
+		player = nbt.getString("Player");	
 	}
 	
 	public void writeToNBT(NBTTagCompound nbt) {
 		nbt.setString("Player", player);
-		for(CoinType coinType : coins.keySet()) {
-			nbt.setInteger("Coin-" + coinType.getName(), coins.get(coinType));
-		}
-		
-		nbt.setTag("Items", InventoryHelper.writeInventoryToNBT(items.toArray(new ItemStack[items.size()])));
 	}
 }
---
+++ src/main/java/ccm/trade_stuffs/inventory/InventoryBankCoins.java
@@ -0,0 +1,10 @@
+package ccm.trade_stuffs.inventory;
+
+import net.minecraft.inventory.InventoryBasic;
+
+public class InventoryBankCoins extends InventoryBasic {
+
+	public InventoryBankCoins(String title, boolean localized, int slots) {
+		super(title, localized, slots);
+	}
+}
