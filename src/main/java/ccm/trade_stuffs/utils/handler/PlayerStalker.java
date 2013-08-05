3f6143a6ac02518911f0076a93ff74b070214f41
Captain Shadows <captain_shadows@aol.com>
2013-08-05 03:24:47 -0400

    ...

--- src/main/java/ccm/trade_stuffs/utils/handler/PlayerStalker.java
+++ src/main/java/ccm/trade_stuffs/utils/handler/PlayerStalker.java
@@ -4,10 +4,12 @@
 package ccm.trade_stuffs.utils.handler;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 
 import cpw.mods.fml.common.IPlayerTracker;
 
+import ccm.trade_stuffs.items.ModItems;
 import ccm.trade_stuffs.utils.lib.Archive;
 
 /**
@@ -18,14 +20,20 @@ import ccm.trade_stuffs.utils.lib.Archive;
  */
 public class PlayerStalker implements IPlayerTracker
 {
-    public static final String PLAYER_NBT = ""
+    public static final String PLAYER_NBT = "CCM.PLAYER.WALLET";
 
     @Override
     public void onPlayerLogin(final EntityPlayer player)
     {
         final NBTTagCompound tag = player.getEntityData();
-        if(!tag.hasKey(Archive.MOD_ID)){
+        if (!tag.hasKey(Archive.MOD_ID))
+        {
             tag.setCompoundTag(Archive.MOD_ID, new NBTTagCompound());
+        }
+        if (!tag.getCompoundTag(Archive.MOD_ID).getBoolean(PLAYER_NBT))
+        {
+            tag.setBoolean(PLAYER_NBT, true);
+            player.inventory.addItemStackToInventory(new ItemStack(ModItems.wallet));
         }
     }
 
