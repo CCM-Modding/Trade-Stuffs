0b8871fac4cafd906ed0622dbcd14d0945c29eda
ElConquistador <itsme.elconquistador@gmail.com>
2013-08-05 08:08:45 +0200

    Added saving and loading to the bank

--- src/main/java/ccm/trade_stuffs/bank/BankAccount.java
+++ src/main/java/ccm/trade_stuffs/bank/BankAccount.java
@@ -4,7 +4,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import ccm.trade_stuffs.api.coins.CoinType;
+import ccm.trade_stuffs.api.coins.CoinTypes;
 import ccm.trade_stuffs.utils.helper.InventoryHelper;
 
 public class BankAccount {
@@ -26,6 +29,7 @@ public class BankAccount {
 			balance = coins.get(coinType);
 		}
 		coins.put(coinType, balance + amount);
+		countCoinBalance();
 	}
 
 	public void removeCoins(CoinType coinType, int amount) {
@@ -34,6 +38,7 @@ public class BankAccount {
 			balance = coins.get(coinType);
 		}
 		coins.put(coinType, (int) Math.min(0, balance - amount));
+		countCoinBalance();
 	}
 
 	public void addItemStack(ItemStack stack) {
@@ -66,5 +71,32 @@ public class BankAccount {
 
 	public int getCoinBalance() {
 		return coinBalance;
+	}
+	
+	public void readFromNBT(NBTTagCompound nbt) {
+		player = nbt.getString("Player");
+		
+		coins = new HashMap<CoinType, Integer>();
+		for(CoinType coinType : CoinTypes.getTypes()) {
+			if(nbt.hasKey("Coin-" + coinType.getName())) {
+				coins.put(coinType, nbt.getInteger("Coin-" + coinType.getName()));
+			}
+		}
+		countCoinBalance();
+		
+		NBTTagList list = nbt.getTagList("Items");
+		ItemStack[] stacks = InventoryHelper.readInventoryFromNBT(list, list.tagCount());		
+		for(int i = 0; i < stacks.length; i++) {
+			items.add(stacks[i]);
+		}
+	}
+	
+	public void writeToNBT(NBTTagCompound nbt) {
+		nbt.setString("Player", player);
+		for(CoinType coinType : coins.keySet()) {
+			nbt.setInteger("Coin-" + coinType.getName(), coins.get(coinType));
+		}
+		
+		nbt.setTag("Items", InventoryHelper.writeInventoryToNBT(items.toArray(new ItemStack[items.size()])));
 	}
 }
--- src/main/java/ccm/trade_stuffs/tileentity/Bank.java
+++ src/main/java/ccm/trade_stuffs/tileentity/Bank.java
@@ -5,6 +5,8 @@ package ccm.trade_stuffs.tileentity;
 
 import java.util.HashMap;
 
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import ccm.trade_stuffs.bank.BankAccount;
 
 /**
@@ -25,5 +27,33 @@ public class Bank extends BaseInventory {
 	@Override
 	public boolean isInvNameLocalized() {
 		return false;
+	}
+	
+	@Override
+	public void readFromNBT(NBTTagCompound nbt) {
+		super.readFromNBT(nbt);
+		if(nbt.hasKey("Accounts")) {
+			NBTTagList list = nbt.getTagList("Accounts");
+			for(int i = 0 ; i < list.tagCount(); i++) {
+				NBTTagCompound tag = (NBTTagCompound) list.tagAt(i);
+				BankAccount account = new BankAccount(tag.getString("Player"));
+				account.readFromNBT(tag);
+				accounts.put(account.player, account);
+			}
+		}
+	}
+	
+	@Override
+	public void writeToNBT(NBTTagCompound nbt) {
+		super.writeToNBT(nbt);
+		NBTTagList list = new NBTTagList();
+		for(BankAccount account : accounts.values()) {
+			if(account != null) {
+				NBTTagCompound tag = new NBTTagCompound();
+				account.writeToNBT(tag);
+				list.appendTag(tag);
+			}
+		}
+		nbt.setTag("Accounts", list);
 	}
 }
