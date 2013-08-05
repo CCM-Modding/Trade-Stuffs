66ab596cbe20e04869582e57b79d85a283a456b6
Captain Shadows <captain_shadows@aol.com>
2013-08-05 11:09:38 -0400

    Fix # 3

--- src/main/java/ccm/trade_stuffs/proxy/CommonProxy.java
+++ src/main/java/ccm/trade_stuffs/proxy/CommonProxy.java
@@ -25,33 +25,55 @@ import ccm.trade_stuffs.utils.lib.Guis;
  * 
  * @author Captain_Shadows
  */
-public abstract class CommonProxy implements IGuiHandler {
+public class CommonProxy implements IGuiHandler
+{
 
-	public abstract void initRenderingStuffs();
+    public void initRenderingStuffs()
+    {}
 
-	public void registerTileEntitys() {
-		GameRegistry.registerTileEntity(TileEntityTradeStation.class, "CCM.TILE.ENTITY.TRADE.STATION");
-		GameRegistry.registerTileEntity(TileEntityBank.class, "CCM.TILE.ENTITY.BANK");
-		GameRegistry.registerTileEntity(TileEntitySafe.class, "CCM.TILE.ENTITY.SAFE");
-	}
+    public void registerTileEntitys()
+    {
+        GameRegistry.registerTileEntity(TileEntityTradeStation.class, "CCM.TILE.ENTITY.TRADE.STATION");
+        GameRegistry.registerTileEntity(TileEntityBank.class, "CCM.TILE.ENTITY.BANK");
+        GameRegistry.registerTileEntity(TileEntitySafe.class, "CCM.TILE.ENTITY.SAFE");
+    }
 
-	@Override
-	public Object getServerGuiElement(final int ID, final EntityPlayer player, final World world, final int x, final int y, final int z) {
-		switch(ID) {
-		case Guis.GUI_TRADE:
-			return new ContainerTrade(player.inventory, (TileEntityTradeStation) world.getBlockTileEntity(x, y, z));
-		case Guis.GUI_BANK:
-			TileEntityBank tile = (TileEntityBank) world.getBlockTileEntity(x, y, z);
-			tile.openChest();
-			return new ContainerBank(player.inventory, tile.currentAccount.getCoins().setBank(tile), tile);
-		case Guis.GUI_WALLET:
-			return new ContainerWallet(player.getCurrentEquippedItem(), player);
-		case Guis.GUI_SAFE:
-			return new ContainerEmpty(player.inventory);
-		case Guis.GUI_SAFE_INVENTORY:
-			return new ContainerSafe(player.inventory, (TileEntitySafe) world.getBlockTileEntity(x, y, z));
-		default:
-			return null;
-		}
-	}
+    @Override
+    public Object getServerGuiElement(final int ID,
+                                      final EntityPlayer player,
+                                      final World world,
+                                      final int x,
+                                      final int y,
+                                      final int z)
+    {
+        switch (ID)
+        {
+            case Guis.GUI_TRADE:
+                return new ContainerTrade(player.inventory,
+                                          (TileEntityTradeStation) world.getBlockTileEntity(x, y, z));
+            case Guis.GUI_BANK:
+                final TileEntityBank tile = (TileEntityBank) world.getBlockTileEntity(x, y, z);
+                tile.openChest();
+                return new ContainerBank(player.inventory, tile.currentAccount.getCoins().setBank(tile), tile);
+            case Guis.GUI_WALLET:
+                return new ContainerWallet(player.getCurrentEquippedItem(), player);
+            case Guis.GUI_SAFE:
+                return new ContainerEmpty(player.inventory);
+            case Guis.GUI_SAFE_INVENTORY:
+                return new ContainerSafe(player.inventory, (TileEntitySafe) world.getBlockTileEntity(x, y, z));
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public Object getClientGuiElement(final int ID,
+                                      final EntityPlayer player,
+                                      final World world,
+                                      final int x,
+                                      final int y,
+                                      final int z)
+    {
+        return null;
+    }
 }
