cc539d4a03cc7f450d74faad9b0b3017c9104374
Captain Shadows <captain_shadows@aol.com>
2013-08-05 04:45:24 -0400

    15 mins Auto Commit for Trade Stuffs

--- src/main/java/ccm/trade_stuffs/inventory/ContainerWallet.java
+++ src/main/java/ccm/trade_stuffs/inventory/ContainerWallet.java
@@ -4,7 +4,9 @@
 package ccm.trade_stuffs.inventory;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
+
 import ccm.trade_stuffs.inventory.slot.SlotInput;
 import ccm.trade_stuffs.items.WalletItem;
 import ccm.trade_stuffs.utils.helper.NBTHelper;
@@ -15,37 +17,66 @@ import ccm.trade_stuffs.utils.helper.NBTHelper;
  * 
  * @author Captain_Shadows
  */
-public class ContainerWallet extends ContainerBase {
+public class ContainerWallet extends ContainerBase
+{
 
-	public InventoryWallet wallet;
+    public InventoryWallet wallet;
 
-	public ContainerWallet(ItemStack item, EntityPlayer player) {
-		super(player.inventory);
-		wallet = new InventoryWallet(item);
-		addPlayerInventory(8, 51);
-		addSlotToContainer(new SlotInput(wallet, 0, 8, 29));
-	}
+    public ContainerWallet(final ItemStack item, final EntityPlayer player)
+    {
+        super(player.inventory);
+        wallet = new InventoryWallet(item);
+        addPlayerInventory(8, 51);
+        addSlotToContainer(new SlotInput(wallet, 0, 8, 29));
+    }
 
-	@Override
-	public void onContainerClosed(EntityPlayer player) {
-		if(!player.worldObj.isRemote) {
-			for(final ItemStack stack : player.inventory.mainInventory) {
-				if(stack != null) {
-					if(NBTHelper.hasTag(stack, WalletItem.openedWallet)) {
-						if(NBTHelper.hasTag(stack, WalletItem.fullWallet)) {
-							NBTHelper.removeTag(stack, WalletItem.fullWallet);
-						}
-						wallet.writeToNBT(stack);
-						NBTHelper.removeTag(stack, WalletItem.openedWallet);
-					}
-				}
-			}
-		}
-		super.onContainerClosed(player);
-	}
+    @Override
+    public void onContainerClosed(final EntityPlayer player)
+    {
+        if (!player.worldObj.isRemote)
+        {
+            for (final ItemStack stack : player.inventory.mainInventory)
+            {
+                if (stack != null)
+                {
+                    if (NBTHelper.hasTag(stack, WalletItem.openedWallet))
+                    {
+                        if (NBTHelper.hasTag(stack, WalletItem.fullWallet))
+                        {
+                            NBTHelper.removeTag(stack, WalletItem.fullWallet);
+                        }
+                        wallet.writeToNBT(stack);
+                        NBTHelper.removeTag(stack, WalletItem.openedWallet);
+                    }
+                }
+            }
+        }
+        super.onContainerClosed(player);
+    }
 
-	@Override
-	public ItemStack transferStackInSlot(EntityPlayer player, int slot) {
-		return null;
-	}
+    @Override
+    public ItemStack transferStackInSlot(final EntityPlayer player, final int index)
+    {
+        ItemStack stack = null;
+        final Slot slot = (Slot) inventorySlots.get(index);
+        if ((slot != null) && slot.getHasStack())
+        {
+            final ItemStack slotStack = slot.getStack();
+            stack = slotStack.copy();
+            if (index < 1)
+            {
+                if (!mergeItemStack(slotStack, 0, 45, true))
+                {
+                    return null;
+                }
+            }
+            else
+                if (!mergeItemStack(slotStack, 0, 0, false))
+                {
+                    return null;
+                }
+
+        }
+        return stack;
+    }
 }
